9853eed9413ed74be328550532e91f3625cd8f5c
Robert Ventura <rventura@lludrigasoft.cat>
2016-12-01 16:26:48 +0100

    Incorporo instal·lació wget i bzip2

--- install_large_db_employess.sh
+++ install_large_db_employess.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
+# VARIABLES NECESSÀRIES
+export DB_USER=usuari
+export DB_PWD=password
+
+yum install -y wget bzip2
 
 # Download Database
-curl https://launchpad.net/test-db/employees-db-1/1.0.6/+download/employees_db-full-1.0.6.tar.bz2 | tar -xjvf file.tar.bz2 -C /tmp/
+cd /tmp/
+wget https://launchpad.net/test-db/employees-db-1/1.0.6/+download/employees_db-full-1.0.6.tar.bz2
+#tar -xjvf file.tar.bz2 -C /tmp/employees_db
 
-# Import Database
-export DB_USER=wordpress
-export DB_PWD=password
+tar -xjvf file.tar.bz2
+cd /tmp/employess_db
 
 mysql -u $DB_USER --password="$DB_PWD" < /tmp/employees.sql
 
 mysql -u $DB_USER --password="$DB_PWD" < /tmp/test_employees_md5.sql
 
+cd /tmp/
+
+rm -rf employees_db
+rm employees_db-full-1.0.6.tar.bz2
