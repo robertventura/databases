35493c8dbadd63da60c7afdac0cf96a6f1462562
Robert Ventura <rventura@lludrigasoft.cat>
2016-12-01 17:00:01 +0100

    Error en el nom del directori

--- install_large_db_employess.sh
+++ install_large_db_employess.sh
@@ -12,7 +12,8 @@ wget https://launchpad.net/test-db/employees-db-1/1.0.6/+download/employees_db-f
 #tar -xjvf file.tar.bz2 -C /tmp/employees_db
 
 tar -xjvf employees_db-full-1.0.6.tar.bz2
-cd /tmp/employess_db
+
+cd /tmp/employees_db
 
 mysql -u $DB_USER --password="$DB_PWD" < /tmp/employees.sql
 
@@ -21,4 +22,4 @@ mysql -u $DB_USER --password="$DB_PWD" < /tmp/test_employees_md5.sql
 cd /tmp/
 
 rm -rf employees_db
-rm employees_db-full-1.0.6.tar.bz2
+rm -f employees_db-full-1.0.6.tar.bz2
