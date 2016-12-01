91f8387293d0b1e63434808228a51bf0726e95a4
Robert Ventura <rventura@lludrigasoft.cat>
2016-12-01 16:53:29 +0100

    El nom del fitxer era erroni

--- install_large_db_employess.sh
+++ install_large_db_employess.sh
@@ -11,7 +11,7 @@ cd /tmp/
 wget https://launchpad.net/test-db/employees-db-1/1.0.6/+download/employees_db-full-1.0.6.tar.bz2
 #tar -xjvf file.tar.bz2 -C /tmp/employees_db
 
-tar -xjvf file.tar.bz2
+tar -xjvf employees_db-full-1.0.6.tar.bz2
 cd /tmp/employess_db
 
 mysql -u $DB_USER --password="$DB_PWD" < /tmp/employees.sql
