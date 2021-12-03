f225c644bc9e65f2e7f8b07250e10a02cfb873ef
Robert Ventura - Home <rventura@sapalomera.cat>
2021-12-03 11:05:59 +0100

    Afegeixo la taula de províncies

--- covid_db/covid_db-schema.sql
+++ covid_db/covid_db-schema.sql
@@ -1,36 +1,18 @@
--- MySQL dump 10.13  Distrib 8.0.17-8, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.21, for Win64 (x86_64)
 --
--- Host: localhost    Database: covid_db
+-- Host: 192.168.56.101    Database: covid_db
 -- ------------------------------------------------------
 -- Server version	8.0.17-8
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-/*!50717 SELECT COUNT(*) INTO @rocksdb_has_p_s_session_variables FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'performance_schema' AND TABLE_NAME = 'session_variables' */;
-/*!50717 SET @rocksdb_get_is_supported = IF (@rocksdb_has_p_s_session_variables, 'SELECT COUNT(*) INTO @rocksdb_is_supported FROM performance_schema.session_variables WHERE VARIABLE_NAME=\'rocksdb_bulk_load\'', 'SELECT 0') */;
-/*!50717 PREPARE s FROM @rocksdb_get_is_supported */;
-/*!50717 EXECUTE s */;
-/*!50717 DEALLOCATE PREPARE s */;
-/*!50717 SET @rocksdb_enable_bulk_load = IF (@rocksdb_is_supported, 'SET SESSION rocksdb_bulk_load = 1', 'SET @rocksdb_dummy_bulk_load = 0') */;
-/*!50717 PREPARE s FROM @rocksdb_enable_bulk_load */;
-/*!50717 EXECUTE s */;
-/*!50717 DEALLOCATE PREPARE s */;
-
---
--- Current Database: `covid_db`
---
-
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `covid_db` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
-
-USE `covid_db`;
 
 --
 -- Table structure for table `casos`
@@ -118,8 +100,11 @@ CREATE TABLE `comarques` (
   `comarca_id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
   `codi` char(3) DEFAULT NULL,
   `nom` varchar(30) DEFAULT NULL,
+  `provincia_id` tinyint(3) unsigned DEFAULT NULL,
   PRIMARY KEY (`comarca_id`),
-  UNIQUE KEY `comarca_codi_UNIQUE` (`codi`)
+  UNIQUE KEY `comarca_codi_UNIQUE` (`codi`),
+  KEY `fk_comarques_provincies` (`provincia_id`),
+  CONSTRAINT `fk_comarques_provincies` FOREIGN KEY (`provincia_id`) REFERENCES `provincies` (`provincia_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -144,6 +129,21 @@ CREATE TABLE `municipis` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Table structure for table `provincies`
+--
+
+DROP TABLE IF EXISTS `provincies`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `provincies` (
+  `provincia_id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
+  `codi` char(2) DEFAULT NULL,
+  `nom` char(10) DEFAULT NULL,
+  PRIMARY KEY (`provincia_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Table structure for table `tipus_casos`
 --
 
@@ -157,10 +157,6 @@ CREATE TABLE `tipus_casos` (
   PRIMARY KEY (`tipus_cas_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-/*!50112 SET @disable_bulk_load = IF (@is_rocksdb_supported, 'SET SESSION rocksdb_bulk_load = @old_rocksdb_bulk_load', 'SET @dummy_rocksdb_bulk_load = 0') */;
-/*!50112 PREPARE s FROM @disable_bulk_load */;
-/*!50112 EXECUTE s */;
-/*!50112 DEALLOCATE PREPARE s */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -171,4 +167,4 @@ CREATE TABLE `tipus_casos` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-07-27 13:40:58
+-- Dump completed on 2021-12-03 10:55:08
